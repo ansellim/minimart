de6c4f5751a33252e78eb1a2d838b46eccc7ee49
Ansel Lim <ansellim@gmail.com>
2021-01-25 14:39:51 +0800

    trying AJAX on products.html/js

--- products.js
+++ products.js
@@ -2,5 +2,7 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open("GET", "https://ansellim.github.io/minimart/products.json");
 ourRequest.onload = function () {
   console.log(ourRequest.responseText);
+  var ourData = ourRequest.responseText;
+  console.log("First product: " + ourData[0].name);
 };
 ourRequest.send();
