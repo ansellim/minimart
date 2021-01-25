4b5ce33aaf6740554077ae9415796c95e925020e
Ansel Lim <ansellim@gmail.com>
2021-01-25 14:43:03 +0800

    trying AJAX on products.html/js

--- products.js
+++ products.js
@@ -1,8 +1,10 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open("GET", "https://ansellim.github.io/minimart/products.json");
-ourRequest.onload = function () {
-  console.log(ourRequest.responseText);
-  var ourData = ourRequest.responseText;
-  console.log("First product: " + ourData[0].name);
-};
-ourRequest.send();
+$(document).ready(function () {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open("GET", "https://ansellim.github.io/minimart/products.json");
+  ourRequest.onload = function () {
+    // console.log(ourRequest.responseText);
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log("First product: " + ourData[0].name);
+  };
+  ourRequest.send();
+});
